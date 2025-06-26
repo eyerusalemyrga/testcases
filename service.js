d0c643eaef5f027dcb7394ce915d8c57972f44af
eyerusalemyrga <eyerusalemykahsay@gmail.com>
2025-06-26 17:42:41 +0300

    Update service.js

--- service.js
+++ service.js
@@ -13,8 +13,8 @@ document.addEventListener('click', function(event) {
     if (
         navLinks &&
         navLinks.classList.contains('show') &&
-        !navLinks.contains(event.target) &&
-        !hamburger.contains(event.target)
+        navLinks.contains(event.target) &&
+        hamburger.contains(event.target)
     ) {
         navLinks.classList.remove('show');
     }
